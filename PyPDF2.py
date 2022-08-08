c579c0a362a84c8ce99588255ca248134c3fa539
vikvo11 <34969699+vikvo11@users.noreply.github.com>
2022-08-09 00:16:01 +0300

    Update PyPDF2.py

--- PyPDF2.py
+++ PyPDF2.py
@@ -1,6 +1,6 @@
 from PyPDF2 import PdfReader #pip install PyPDF2
 
-reader = PdfReader("zadost_prijeti.pdf")
+reader = PdfReader("test_tables.pdf")
 text = ""
 for page in reader.pages:
     text += page.extract_text() + "\n"
