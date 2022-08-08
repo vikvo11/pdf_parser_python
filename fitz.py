a578cc3e240a1b061383d03c33c0a9cefa42ac35
vikvo11 <34969699+vikvo11@users.noreply.github.com>
2022-08-09 00:16:23 +0300

    Update fitz.py

--- fitz.py
+++ fitz.py
@@ -1,6 +1,6 @@
 import fitz # install using: pip install PyMuPDF
 
-with fitz.open("zadost_prijeti.pdf") as doc:
+with fitz.open("test_tables.pdf") as doc:
     text = ""
     for page in doc:
         text += page.get_text()
